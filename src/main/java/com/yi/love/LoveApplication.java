203953b0f05847eb095e745f3a9c45d393144313
YI <ilovey_hwy@163.com>
2018-09-10 11:00:39 +0800

    :tada: 初始化提交

---
+++ src/main/java/com/yi/love/LoveApplication.java
@@ -0,0 +1,17 @@
+package com.yi.love;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 爱你（づ￣3￣）づ╭❤～
+ * @author YI
+ * @date 2018-9-10 10:48:08
+ */
+@SpringBootApplication
+public class LoveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoveApplication.class, args);
+	}
+}
